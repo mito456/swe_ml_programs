edd278f014a5e3d230600f975d8da671e2ba982f
ananya <mitochondria4566@gmail.com>
2026-08-12 12:17:49 +0530

    Fix liner regression code

--- liner_reg.py
+++ liner_reg.py
@@ -13,3 +13,27 @@ bmi=linear_model.LinearRegression()
 bmi.fit(height,weight)
 X_height=[[13.0]]
 print(bmi.predict(X_height))
+"""Linear regression example using height and weight data."""
+
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+
+def main():
+    """Train a linear regression model and predict weight."""
+    height = [[4.0], [5.0], [6.0], [7.0], [8.0], [9.0], [10.0]]
+    weight = [8, 10, 12, 14, 16, 18, 20]
+
+    plt.scatter(height, weight, color="blue")
+    plt.xlabel("height")
+    plt.ylabel("weight")
+
+    model = linear_model.LinearRegression()
+    model.fit(height, weight)
+
+    x_height = [[13.0]]
+    print(model.predict(x_height))
+
+
+if __name__ == "__main__":
+    main()
